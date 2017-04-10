99f48b554ba58e0c628c70fb0f1b0bef3a012ea9
Ivano Pagano <pagoda_5b@hotmail.com>
2017-04-10 11:24:17 +0200

    Add tests and implementation of the scala solution for step02

---
+++ src/test/scala/Step02Spec.scala
@@ -0,0 +1,23 @@
+package io.evilcorp.step02
+
+import org.scalatest._
+
+class Step02Spec extends WordSpec with Matchers {
+
+	val censored = Set("nice", "pony", "sun", "light", "fun", "happy", "funny", "joy")
+	val input = "Such a nice day with a bright sun, makes me happy"
+	val expected = "Such a XXXX day with a bright XXX, makes me XXXXX"
+
+	"The imperative censorship function" should {
+		"censor a list of provided words" in {
+			Step02ScalaImperative(censored)(input) shouldBe expected
+		}
+	}
+
+	"The functional censorship function" should {
+		"censor a list of provided words" in {
+			Step02ScalaFunctional(censored)(input) shouldBe expected
+		}
+	}
+
+}
