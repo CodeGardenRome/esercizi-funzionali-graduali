e13095ea65675134cb71b61ba27794ddae65d366
vitalijzad <vitalij_zad@libero.it>
2017-07-22 08:06:27 +0200

    Added an imperative and a stream solution for step 02

--- src/main/java/io/evilcorp/step02/Step02Javaslang.java
+++ src/main/java/io/evilcorp/step02/Step02Javaslang.java
@@ -10,9 +10,9 @@ public class Step02Javaslang implements Function2<Set<String>, String, String> {
 
     @Override
     public String apply(Set<String> censored, String text) {
-    	return censored.foldLeft(CharSeq.of(text), 
-    		(txt, verboten) -> txt.replaceAll(verboten, Marked.apply(verboten))
-    	).toString();
+        return censored.foldLeft(CharSeq.of(text),
+                (txt, verboten) -> txt.replaceAll(verboten, Marked.apply(verboten))
+        ).toString();
     }
 
     private final char marker = 'X';
--- src/test/java/io/evilcorp/Tester.java
+++ src/test/java/io/evilcorp/Tester.java
@@ -4,9 +4,16 @@ import io.evilcorp.step01.Step01Imperative;
 import io.evilcorp.step01.Step01Javaslang;
 import io.evilcorp.step01.Step01StreamsExplicit;
 import io.evilcorp.step01.Step01StreamsImplicit;
+import io.evilcorp.step02.Step02Imperative;
+import io.evilcorp.step02.Step02Javaslang;
+import io.evilcorp.step02.Step02Streams;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.BiFunction;
 import java.util.function.UnaryOperator;
 
 public class Tester {
@@ -19,6 +26,12 @@ public class Tester {
         step01Test(new Step01Javaslang());
     }
 
+    @Test
+    public void step02(){
+        step02Test(new Step02Imperative());
+        step02Test(new Step02Streams());
+    }
+
     public void step01Test(UnaryOperator<String> unaryOperator){
         String input = "You are a nice person";
         String expected = "You are a XXXX person";
@@ -26,5 +39,11 @@ public class Tester {
         Assert.assertEquals(unaryOperator.getClass().toString(), expected, output);
     }
 
-
+    public void step02Test(BiFunction<Set<String>, String, String> biFunction){
+        String input = "Such a nice day with a bright sun, makes me happy";
+        String expected = "Such a XXXX day with a bright XXX, makes me XXXXX";
+        Set<String> blacklistedWords = new HashSet<String>(Arrays.asList("nice", "pony", "sun", "light", "fun", "happy", "funny", "joy"));
+        String output = biFunction.apply(blacklistedWords, input);
+        Assert.assertEquals(biFunction.getClass().toString(), expected, output);
+    }
 }
