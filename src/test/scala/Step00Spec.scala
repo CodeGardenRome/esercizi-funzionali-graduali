1054c686d8175fb485145f51ab996aeb2622d227
Ivano Pagano <pagoda_5b@hotmail.com>
2017-08-29 23:55:46 +0200

    Test: prepara lo scheletro dei test che il codice dovra' soddisfare

---
+++ src/test/scala/Step00Spec.scala
@@ -0,0 +1,136 @@
+package io.evilcorp.step00
+
+import org.scalatest._
+
+class Step00Spec extends WordSpec with Matchers {
+
+  val men = Array("Tizio", "Caio", "Sempronio", "Mevio", "Filano" e "Calpurnio")
+  val women = Array("Anna", "Carla", "Angela", "Chiara", "Emma", "Maria", "Sara")
+
+  "The solution" when {
+
+    "using the imperative approach" should {
+
+      "produce 1, 2, 3" in {
+        
+      }
+
+      "produce all even numbers between 1 and 10" in {
+
+      }
+
+      "produce all multiples of 7 in the range [1, 100]" in {
+
+      }
+
+      "from the array of men find those starting with 'C'" in {
+
+      }
+
+      "compute the average of multiples of 8 in the range [1, 100]" in {
+        
+      }
+
+      "compute the sum of multiples of 6 in the range [1, 100]" in {
+
+      }
+
+      "return the sorted array of men" in {
+
+      }
+
+      "return the sorted array of men as a list to be lately consumed" in {
+
+      }
+
+      "find any number in the range [1, 100] that is dvided by 41 and print it" in {
+
+      }
+
+      "produce a string containing the list of men, comma separated" in {
+
+      }
+
+      "produce a set from teh array of men" in {
+
+      }
+
+      "group women's names by length" in {
+
+      }
+
+      "print women's names length" in {
+        
+      }
+
+      "print women's first letter" in {
+
+      }
+
+    }
+
+    "using the functional approach" should {
+
+      "produce 1, 2, 3" in {
+
+      }
+
+      "produce all even numbers between 1 and 10" in {
+
+      }
+
+      "produce all multiples of 7 in the range [1, 100]" in {
+
+      }
+
+      "from the array of men find those starting with 'C'" in {
+
+      }
+
+      "compute the average of multiples of 8 in the range [1, 100]" in {
+        
+      }
+
+      "compute the sum of multiples of 6 in the range [1, 100]" in {
+
+      }
+
+      "return the sorted array of men" in {
+
+      }
+
+      "return the sorted array of men as a list to be lately consumed" in {
+
+      }
+
+      "find any number in the range [1, 100] that is dvided by 41 and print it" in {
+
+      }
+
+      "produce a string containing the list of men, comma separated" in {
+
+      }
+
+      "produce a set from teh array of men" in {
+
+      }
+
+      "group women's names by length" in {
+
+      }
+
+      "print women's names length" in {
+        
+      }
+
+      "print women's first letter" in {
+
+      }
+
+
+
+    }
+
+  }
+
+}
