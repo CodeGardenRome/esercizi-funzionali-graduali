d716511da9b216124ad9607ebc2bf41973c70a51
vitalijzad <vitalij_zad@libero.it>
2017-09-07 22:41:59 +0200

    Prima parte kata 0 in JavaScript

--- js/src/App.js
+++ js/src/App.js
@@ -1,4 +1,33 @@
 
+var _ = require('lodash');
+
+export const UOMINI = ["Tizio", "Caio", "Sempronio", "Mevio", "Filano", "Calpurnio"];
+export const DONNE = ["Anna", "Carla", "Angela", "Chiara", "Emma", "Maria", "Sara"];
+
 export const pippo = () => {
     return true;
+};
+
+export const kataZeroA = () => {
+    return _.range(1,4);
+};
+
+export const kataZeroB = () => {
+    return _.range(2,10,2)
+};
+
+export const kataZeroC = () => {
+    return _.range(7,100,7)
+};
+
+export const kataZeroD = () => {
+    return _.filter(UOMINI, (s) => s.startsWith("C"));
+};
+
+export const kataZeroE = () => {
+    return _.mean(_.range(8,100,8));
+};
+
+export const kataZeroF = () => {
+    return _.sum(_.range(6,1000,6));
 };
--- js/src/App.test.js
+++ js/src/App.test.js
@@ -1,6 +1,30 @@
 import expect from 'expect'
-import {pippo} from './App'
+import {pippo,kataZeroA,kataZeroB,kataZeroC,kataZeroD,kataZeroE,kataZeroF} from './App'
 
 it('pippo torna true', () => {
     expect(pippo()).toBe(true);
 });
+
+it('kataZeroA restituisce [1,2,3]', () =>{
+    expect(kataZeroA()).toEqual([1,2,3]);
+})
+
+it('kataZeroB restituisce i pari minori di 10', () =>{
+    expect(kataZeroB()).toEqual([ 2, 4, 6, 8 ]);
+})
+
+it('kataZeroC restituisce i multipli di 7 minori di 100', () =>{
+    expect(kataZeroC()).toEqual([ 7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98 ]);
+})
+
+it('kataZeroD restituisce gli uomini che cominciano per C', () =>{
+    expect(kataZeroD()).toEqual([ 'Caio', 'Calpurnio' ]);
+})
+
+it('kataZeroE restituisce la media dei multipli di 8 inferiori a 100', () =>{
+    expect(kataZeroE()).toEqual(52);
+})
+
+it('kataZeroF restituisce la somma dei multipli di 6 inferiori a 100', () =>{
+    expect(kataZeroF()).toEqual(83166);
+})
