95dc4a00a7802807f4a7a5f3b73304dd29304681
Federico Caprari <korsmakolnikov@gmail.com>
2017-09-08 21:33:02 +0200

    fine implementazione js, piccole correzioni php

--- js/src/App.js
+++ js/src/App.js
@@ -1,16 +1,20 @@
 import _ from 'lodash';
-import {List, Range} from 'immutable';
+import {
+    List,
+    Range,
+    Set
+} from 'immutable';
 
 export const ZeroA = () => {
-    return _.range(1,4);
+    return _.range(1, 4);
 };
 
 export const ZeroB = () => {
-    return _.range(2,10,2);
+    return _.range(2, 10, 2);
 };
 
 export const ZeroC = () => {
-    return _.range(7,100,7);
+    return _.range(7, 100, 7);
 };
 
 export const ZeroD = uomini => {
@@ -18,13 +22,38 @@ export const ZeroD = uomini => {
 };
 
 export const ZeroE = () => {
-    return _.mean(_.range(8,100,8));
+    return _.mean(_.range(8, 100, 8));
 };
 
 export const ZeroF = () => {
-    return _.sum(_.range(6,1000,6));
+    return _.sum(_.range(6, 1000, 6));
 };
 
 export const ZeroG = uomini => {
     return List(uomini).sort();
+};
+
+export const ZeroH = () => {
+    const range = Range(41, 1000, 41);
+    return range.get(_.random(0, range.count() - 1));
+};
+
+export const ZeroI = uomini => {
+    return List(uomini).join(', ');
+};
+
+export const ZeroJ = uomini => {
+    return Set(uomini);
+};
+
+export const ZeroK = donne => {
+    return List(donne).groupBy(x => x.length.toString());
+};
+
+export const ZeroL = donne => {
+    return List(donne).map(x => x.length);
+};
+
+export const ZeroM = donne => {
+    return List(donne).map(x => x[0]);
 };
--- js/src/App.test.js
+++ js/src/App.test.js
@@ -1,38 +1,71 @@
 import expect from 'expect';
 import * as kata from './App';
-import {List} from 'immutable';
+import {
+    List,
+    Set,
+    OrderedMap
+} from 'immutable';
 import freeze from 'deep-freeze';
 
 export const UOMINI = ["Tizio", "Caio", "Sempronio", "Mevio", "Filano", "Calpurnio"];
 export const DONNE = ["Anna", "Carla", "Angela", "Chiara", "Emma", "Maria", "Sara"];
 
 
-it('kataZeroA restituisce [1,2,3]', () => {
-    expect(kata.ZeroA()).toEqual([1,2,3]);
+it('kata.ZeroA restituisce [1,2,3]', () => {
+    expect(kata.ZeroA()).toEqual([1, 2, 3]);
 });
 
-it('kataZeroB restituisce i pari minori di 10', () => {
-    expect(kata.ZeroB()).toEqual([ 2, 4, 6, 8 ]);
+it('kata.ZeroB restituisce i pari minori di 10', () => {
+    expect(kata.ZeroB()).toEqual([2, 4, 6, 8]);
 });
 
-it('kataZeroC restituisce i multipli di 7 minori di 100', () => {
-    expect(kata.ZeroC()).toEqual([ 7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98 ]);
+it('kata.ZeroC restituisce i multipli di 7 minori di 100', () => {
+    expect(kata.ZeroC()).toEqual([7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98]);
 });
 
-it('kataZeroD restituisce gli uomini che cominciano per C', () => {
-    expect(kata.ZeroD(freeze(UOMINI))).toEqual([ 'Caio', 'Calpurnio' ]);
+it('kata.ZeroD restituisce gli uomini che cominciano per C', () => {
+    expect(kata.ZeroD(freeze(UOMINI))).toEqual(['Caio', 'Calpurnio']);
 });
 
-it('kataZeroE restituisce la media dei multipli di 8 inferiori a 100', () => {
+it('kata.ZeroE restituisce la media dei multipli di 8 inferiori a 100', () => {
     expect(kata.ZeroE()).toEqual(52);
 });
 
-it('kataZeroF restituisce la somma dei multipli di 6 inferiori a 100', () => {
+it('kata.ZeroF restituisce la somma dei multipli di 6 inferiori a 100', () => {
     expect(kata.ZeroF()).toEqual(83166);
 });
 
-it('kataZeroG restituisce una lista ordinata di numi di uomini', () => {
+it('kata.ZeroG restituisce una lista ordinata di nomi di uomini', () => {
     expect(kata.ZeroG(freeze(UOMINI)))
         .toEqual(
             List(['Caio', 'Calpurnio', 'Filano', 'Mevio', 'Sempronio', 'Tizio']));
+});
+
+it('kata.ZeroH restituisce un elemento a caso dei multipli di 41', () => {
+    const res = kata.ZeroH();
+    expect(res % 41).toEqual(0);
+});
+
+it('kata.ZeroF trasforma l\'elenco dei nomi di uomini in una stringa separati da virgola', () => {
+    expect(kata.ZeroI(freeze(UOMINI))).toEqual("Tizio, Caio, Sempronio, Mevio, Filano, Calpurnio");
+});
+
+it('kata.ZeroJ ritorna un set di nomi di uomini', () => {
+    expect(kata.ZeroJ(freeze(UOMINI))).toEqual(Set(UOMINI));
+});
+
+it('kata.ZeroK ritorna i nomi delle donne raggruppati per la loro lunghezza', () => {
+    expect(kata.ZeroK(DONNE)).toEqual(OrderedMap({
+        4: List(["Anna", "Emma", "Sara"]),
+        5: List(["Carla", "Maria"]),
+        6: List(["Angela", "Chiara"])
+    }));
+});
+
+it('kata.ZeroL ritorna una lista con la lungezza di ogni nome di donna', () => {
+    expect(kata.ZeroL(DONNE)).toEqual(List([4, 5, 6, 6, 4, 5, 4]));
+});
+
+it('kata.ZeroM ritorna una lista con la sequenza delle iniziali del nome delle donne', () => {
+    expect(kata.ZeroM(DONNE)).toEqual(List(['A', 'C', 'A', 'C', 'E', 'M', 'S']));
 });
