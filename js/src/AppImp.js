b665e2234497644af83e512fed8cf93fbea9340b
vitalijzad <vitalij_zad@libero.it>
2017-10-04 18:56:35 +0200

    Implementazione imperativa JavaScript

--- js/src/AppImp.js
+++ js/src/AppImp.js
@@ -1,3 +1,5 @@
+import Set from 'immutable';
+
 export const ZeroA = () => {
     let out = [];
     for (let i = 1; i <= 3; i++) {
@@ -6,3 +8,105 @@ export const ZeroA = () => {
 
     return out;
 };
+
+export const ZeroB = () => {
+    let out = [];
+    for(let i=2; i < 10; i+=2){
+        out.push(i);
+    }
+
+    return out;
+};
+
+export const ZeroC = () => {
+    let out = [];
+    for(let i=7; i <= 100; i+=7){
+        out.push(i);
+    }
+
+    return out;
+}
+
+export const ZeroD = (uomini) => {
+    let out = [];
+    for(let i=0; i <= uomini.length; i++){
+        let s = uomini[i];
+        if(s.startsWith("C")){
+            out.push(s);
+        }
+    }
+
+    return out;
+}
+
+export const ZeroE = () => {
+    let out = [];
+    let count = 0;
+    let sum = 0;
+    for(let i=8; i<100; i+=8){
+        count++;
+        sum+=i;
+    }
+    return sum/count;
+}
+
+export const ZeroF = () => {
+    let sum = 0;
+    for(let i=6; i<1000; i+=6){
+        sum += i;
+    }
+    return sum;
+}
+
+export const ZeroG = uomini => {
+    return List(uomini).sort();
+}
+
+export const ZeroH = () => {
+    let out = [];
+    for(let i=41;i<1000;i+=41){
+        out.push(i);
+    }
+    let j = Math.floor(Math.random()*out.lenght);
+    return out[j];
+}
+
+export const ZeroI = uomini => {
+    let out = "";
+    for(let i=0; i < uomini.lenght; i++){
+        if(i!=0){
+            out += ', ';
+        }
+        out += uomini[i];
+    }
+    return out;
+}
+
+export const ZeroJ = uomini => {
+    return Set(uomini);
+}
+
+export const ZeroK = donne => {
+    let out = {};
+    for(let i=0; i < donne.lenght; i++){
+        let donna = donne[i];
+        out[donna.lenght] += donna;
+    }
+    return out;
+}
+
+export const ZeroL = donne => {
+    let out = [];
+    for(let i=0; i<donne.lenght; i++){
+        out.push(donne[i].lenght);
+    }
+    return out;
+}
+
+export const ZeroM = donne => {
+    let out = [];
+    for(let i=0; i<donne.lenght; i++){
+        out.push(donne[i][0]);
+    }
+    return out;
+}
--- js/src/AppImp.test.js
+++ js/src/AppImp.test.js
@@ -8,3 +8,58 @@ export const DONNE = ["Anna", "Carla", "Angela", "Chiara", "Emma", "Maria", "Sar
 it('kata.ZeroA restituisce [1,2,3]', () => {
     expect(kata.ZeroA()).toEqual([1, 2, 3]);
 });
+
+it('kata.ZeroB restituisce i pari minori di 10', () => {
+    expect(kata.ZeroB()).toEqual([2, 4, 6, 8]);
+});
+
+it('kata.ZeroC restituisce i multipli di 7 minori di 100', () => {
+    expect(kata.ZeroC()).toEqual([7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98]);
+});
+
+it('kata.ZeroD restituisce gli uomini che cominciano per C', () => {
+    expect(kata.ZeroD(UOMINI)).toEqual(['Caio', 'Calpurnio']);
+});
+
+it('kata.ZeroE restituisce la media dei multipli di 8 inferiori a 100', () => {
+    expect(kata.ZeroE()).toEqual(52);
+});
+
+it('kata.ZeroF restituisce la somma dei multipli di 6 inferiori a 100', () => {
+    expect(kata.ZeroF()).toEqual(83166);
+});
+
+it('kata.ZeroG restituisce una lista ordinata di nomi di uomini', () => {
+    expect(kata.ZeroG(UOMINI))
+        .toEqual(
+            ['Caio', 'Calpurnio', 'Filano', 'Mevio', 'Sempronio', 'Tizio']);
+});
+
+it('kata.ZeroH restituisce un elemento a caso dei multipli di 41', () => {
+    const res = kata.ZeroH();
+    expect(res % 41).toEqual(0);
+});
+
+it('kata.ZeroF trasforma l\'elenco dei nomi di uomini in una stringa separati da virgola', () => {
+    expect(kata.ZeroI(UOMINI)).toEqual("Tizio, Caio, Sempronio, Mevio, Filano, Calpurnio");
+});
+
+it('kata.ZeroJ ritorna un set di nomi di uomini', () => {
+    expect(kata.ZeroJ(UOMINI)).toEqual(Set(UOMINI));
+});
+
+it('kata.ZeroK ritorna i nomi delle donne raggruppati per la loro lunghezza', () => {
+    expect(kata.ZeroK(DONNE)).toEqual({
+        4: ["Anna", "Emma", "Sara"],
+        5: ["Carla", "Maria"],
+        6: ["Angela", "Chiara"]
+    });
+});
+
+it('kata.ZeroL ritorna una lista con la lungezza di ogni nome di donna', () => {
+    expect(kata.ZeroL(DONNE)).toEqual([4, 5, 6, 6, 4, 5, 4]);
+});
+
+it('kata.ZeroM ritorna una lista con la sequenza delle iniziali del nome delle donne', () => {
+    expect(kata.ZeroM(DONNE)).toEqual(['A', 'C', 'A', 'C', 'E', 'M', 'S']);
+});
