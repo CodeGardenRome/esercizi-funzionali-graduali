88d75755a92eafb8a89567caf5c12bf3bd7e32ff
vitalijzad <vitalij_zad@libero.it>
2017-10-23 19:18:49 +0200

    Ultimi step

--- haskell/src/KataZero/KataZero.hs
+++ haskell/src/KataZero/KataZero.hs
@@ -1,16 +1,18 @@
 module KataZero.KataZero (
   stepA,stepB,stepC,stepD,stepE,
-  stepF,stepG,stepH,stepI
+  stepF,stepG,stepH,stepI,stepJ,
+  stepK,stepL,stepM
 )  where
 
+import Prelude as P
 import Data.Char
 import Data.List
+import Data.Set as S
+import Data.Map as M
 import System.Random
 
 stepA :: [Int]
 stepA = [1..3]
-
--- TODO Risolvere questi esercizi usando filter e non le list comprehension
 
 stepB :: [Int]
 stepB = [n | n <- [1..10], mod n 2 == 0]
@@ -19,7 +21,8 @@ stepC :: [Int]
 stepC = [n | n <- [1..100], mod n 7 == 0]
 
 stepD :: [String] -> [String]
-stepD xs =  [u | u <- xs, isPrefixOf "C" u] -- Alternativa: filter (isPrefixOf "C") uomini
+stepD xs =  [u | u <- xs, isPrefixOf "C" u]
+-- Alternativa: stepD xs = filter (isPrefixOf "C") xs
 
 average :: [Int] -> Double
 average xs = realToFrac (sum xs) / genericLength xs
@@ -33,6 +36,7 @@ stepF = sum [n | n <- [1..1000], mod n 6 == 0]
 stepG :: [String] -> [String]
 stepG xs = sort xs
 
+-- Restituisce un elemento casuale da una lista
 atRandIndex :: [a] -> IO a  -- note that this is gives itself an IO action
 atRandIndex l = do
     i <- randomRIO (0, length l - 1)
@@ -43,3 +47,19 @@ stepH = atRandIndex [n | n <- [1..1000], mod n 41 == 0]
 
 stepI :: [String] -> String
 stepI xs = intercalate ", " xs
+
+stepJ :: [String] -> Set String
+stepJ xs = S.fromList xs
+
+stepK :: [String] -> Map Int String
+stepK xs = M.fromList $ generateList xs
+
+generateList :: [String] -> [(Int,String)]
+generateList (x:xs) = (length(x), x) : generateList xs
+generateList [] = []
+
+stepL :: [String] -> [Int]
+stepL xs = P.map length xs
+
+stepM :: [String] -> [String]
+stepM xs = P.map (take 1) xs
--- haskell/test/KataZero/KataZeroSpec.hs
+++ haskell/test/KataZero/KataZeroSpec.hs
@@ -1,6 +1,8 @@
 module KataZero.KataZeroSpec (main, spec) where
 
 import Test.Hspec
+import Data.Set as S
+import Data.Map as M
 import KataZero.KataZero
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
@@ -11,6 +13,7 @@ main = hspec spec
 spec :: Spec
 spec = do
   let uomini = ["Tizio", "Caio", "Sempronio", "Mevio", "Filano", "Calpurnio"]
+  let donne = ["Anna", "Carla", "Angela", "Chiara", "Emma", "Maria", "Sara"]
   describe "KataZero" $ do
     it "stepA" $ do
       stepA `shouldBe` [1,2,3]
@@ -31,3 +34,11 @@ spec = do
       shouldSatisfy r (\n -> mod n 41 == 0)
     it "stepI" $ do
       shouldBe (stepI uomini) "Tizio, Caio, Sempronio, Mevio, Filano, Calpurnio"
+    it "stepJ" $ do
+      shouldBe (stepJ uomini) (S.fromList uomini)
+    it "stepK" $ do
+      shouldBe (stepK donne) (M.fromList [(4,"Anna"),(5,"Carla"),(6,"Angela"),(6,"Chiara"),(4,"Emma"),(5,"Maria"),(4,"Sara")])
+    it "stepL" $ do
+      shouldBe (stepL donne) [4,5,6,6,4,5,4]
+    it "stepM" $ do
+      shouldBe (stepM donne) ["A","C","A","C","E","M","S"]
