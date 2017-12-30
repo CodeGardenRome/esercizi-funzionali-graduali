aee2f8a11c41ee57a46d0fa06426ddf1ff364642
vitalijzad <vitalij_zad@libero.it>
2017-12-30 14:31:23 +0100

    Arrivato fino al passo D

--- haskell/src/LambdaGardenDue.hs
+++ haskell/src/LambdaGardenDue.hs
@@ -1,16 +1,17 @@
 module LambdaGardenDue(
   aggrega, vincitore,
   cellaSuccessivaRighe, cellaSuccessivaColonne, cellaSuccessivaDiagonaleMaggiore, cellaSuccessivaDiagonaleMinore,
-  generaVettore
+  generaVettore, generaVettoriRiga
 ) where
+
+import Data.List --Serve per transpose
 
 aggrega :: Char -> Char -> Char
 aggrega 'X' 'X' = 'X'
 aggrega 'O' 'O' = 'O'
 aggrega x y = ' '
 
-prova = foldl aggrega ' ' ['X','X','X']
-
+vincitore :: [Char] -> Char
 vincitore xs = if foldl aggrega 'X' xs == 'X'
   then 'X'
   else if foldl aggrega 'O' xs == 'O'
@@ -18,10 +19,10 @@ vincitore xs = if foldl aggrega 'X' xs == 'X'
     else ' '
 
 cellaSuccessivaRighe :: (Int, Int) -> (Int, Int)
-cellaSuccessivaRighe (x,y) = (x+1,y)
+cellaSuccessivaRighe (x,y) = (x,y+1)
 
 cellaSuccessivaColonne :: (Int, Int) -> (Int, Int)
-cellaSuccessivaColonne (x,y) = (x,y+1)
+cellaSuccessivaColonne (x,y) = (x+1,y)
 
 cellaSuccessivaDiagonaleMaggiore :: (Int, Int) -> (Int, Int)
 cellaSuccessivaDiagonaleMaggiore (x,y) = (x+1,y+1)
@@ -29,8 +30,35 @@ cellaSuccessivaDiagonaleMaggiore (x,y) = (x+1,y+1)
 cellaSuccessivaDiagonaleMinore :: (Int, Int) -> (Int, Int)
 cellaSuccessivaDiagonaleMinore (x,y) = (x-1,y+1)
 
-generaVettore :: [[Int]] -> (Int,Int) -> ((Int, Int) -> (Int, Int)) -> [Int]
+generaVettore :: [[Char]] -> (Int,Int) -> ((Int, Int) -> (Int, Int)) -> [Char]
 generaVettore matrice (x,y) funSuccessore =
-  if (x <= length matrice) && (y <= length (matrice !! x))
+  if x>=0 && y>=0 && (x < length matrice) && (y < length (matrice !! x))
     then (matrice !! x !! y) : (generaVettore matrice (funSuccessore (x,y)) funSuccessore)
+    else []
+
+aggregaVincitore :: Char -> Char -> Char
+aggregaVincitore 'X' ' ' = 'X'
+aggregaVincitore ' ' 'X' = 'X'
+aggregaVincitore 'O' ' ' = 'O'
+aggregaVincitore ' ' 'O' = 'O'
+aggregaVincitore ' ' ' ' = ' '
+
+trovaVincitoreRighe :: [[Char]] -> Char
+trovaVincitoreRighe matrice = foldr aggregaVincitore ' '  (map vincitore matrice)
+
+trovaVincitoreColonne :: [[Char]] -> Char
+trovaVincitoreColonne matrice = foldr aggregaVincitore ' '  (map vincitore (transpose matrice))
+
+generaVettoriRiga :: [[Char]] -> (Int,Int) -> [[Char]]
+generaVettoriRiga matrice (x,y) =
+  if x>=0 && y>=0 && (x < length matrice) && (y < length (matrice !! x))
+    then generaVettore matrice (x,y) cellaSuccessivaRighe :
+    generaVettoriRiga matrice (cellaSuccessivaColonne (x,y))
+    else []
+
+generaVettoriColonna :: [[Char]] -> (Int,Int) -> [[Char]]
+generaVettoriColonna matrice (x,y) =
+  if x>=0 && y>=0 && (x < length matrice) && (y < length (matrice !! x))
+    then generaVettore matrice (x,y) cellaSuccessivaColonne :
+    generaVettoriRiga matrice (cellaSuccessivaRighe (x,y))
     else []
--- haskell/test/LambdaGardenDueSpec.hs
+++ haskell/test/LambdaGardenDueSpec.hs
@@ -8,9 +8,23 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  let matrice = [ [' ', 'O', 'X'], [' ', 'X', 'O'], ['X', ' ', ' '] ]
   describe "Esercizio1" $ do
     it "passoA" $ do
-      vincitore ['X','X','X'] `shouldBe` 'X'
-      vincitore ['O','O','O'] `shouldBe` 'O'
-      vincitore ['O','O','X'] `shouldBe` ' '
-      vincitore ['O',' ','X'] `shouldBe` ' '
+      shouldBe (vincitore ['X','X','X']) 'X'
+      shouldBe (vincitore ['O','O','O']) 'O'
+      shouldBe (vincitore ['O','O','X']) ' '
+      shouldBe (vincitore ['O',' ','X']) ' '
+    it "passoB" $ do
+      shouldBe (cellaSuccessivaRighe (2,1) ) (2,2)
+      shouldBe (cellaSuccessivaColonne (1,1) ) (2,1)
+      shouldBe (cellaSuccessivaDiagonaleMaggiore (1,1)) (2,2)
+      shouldBe ( cellaSuccessivaDiagonaleMinore (2,0)) (1,1)
+    it "passoC" $ do
+      shouldBe (generaVettore matrice (0,0) cellaSuccessivaRighe) [' ', 'O', 'X']
+      shouldBe (generaVettore matrice (0,0) cellaSuccessivaColonne) [' ', ' ', 'X']
+      shouldBe (generaVettore matrice (0,0) cellaSuccessivaDiagonaleMaggiore) [' ', 'X', ' ']
+      shouldBe (generaVettore matrice (2,0) cellaSuccessivaDiagonaleMinore) ['X', 'X', 'X']
+    it "passoD" $ do
+      shouldBe (generaVettoriRiga matrice (0,0)) matrice
+    --it "passoE" $ do
